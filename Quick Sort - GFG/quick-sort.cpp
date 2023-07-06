42ce4839f8e7a746d524e530dfafbac6c8fc5837
omkumarjha <88018877+omkumarjha@users.noreply.github.com>
2023-07-06 21:03:18 +0530

    Added solution - LeetHub

---
+++ Quick Sort - GFG/quick-sort.cpp
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to sort an array using quick sort algorithm.
+    void quickSort(int arr[], int low, int high)
+    {
+        int start = low;
+        int end = high;
+        
+        if(start >= end){
+            return;
+        }
+        else{
+            int p = partition(arr,start,end);
+            
+            quickSort(arr,start,p-1);
+            
+            quickSort(arr,p+1,end);
+        }
+    }
+    
+    public:
+    int partition (int arr[], int low, int high){
+        
+        int start = low;
+        int end = high;
+        
+        int pivot = arr[start];
+        int count = 0;
+        
+        for(int i = start+1; i <= end; i++){
+            if(arr[i] <= pivot){
+                count++;
+            }
+        }
+        
+        int pivotIndex = start + count;
+        swap(arr[start],arr[pivotIndex]);
+        
+        int i = start , j = end;
+        
+        while(i < pivotIndex && j > pivotIndex){
+            while(i < pivotIndex && j > pivotIndex && arr[i] <= arr[pivotIndex]){
+                i++;
+            }
+            while(i < pivotIndex && j > pivotIndex && arr[j] > arr[pivotIndex]){
+                j--;
+            }
+            if(i < pivotIndex && j > pivotIndex){
+                swap(arr[i++],arr[j--]);
+            }
+        }
+        
+        return pivotIndex;
+       
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int arr[1000],n,T,i;
+    scanf("%d",&T);
+    while(T--){
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+      Solution ob;
+    ob.quickSort(arr, 0, n-1);
+    printArray(arr, n);
+    }
+    return 0;
+}
+// } Driver Code Ends
