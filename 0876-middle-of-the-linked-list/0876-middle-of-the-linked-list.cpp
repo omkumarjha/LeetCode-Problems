6d30c346bc669a79a14a643af9f204ac394489c1
omkumarjha <88018877+omkumarjha@users.noreply.github.com>
2023-07-23 10:23:59 +0530

    Time: 5 ms (37.02%), Space: 7 MB (40.43%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -10,6 +10,8 @@
  */
 class Solution {
 public:
+    // Yeh one of the approach hai jisme hum LL ki length calculate karke middle node pata kar rahe hai 
+    /*
     ListNode* middleNode(ListNode* head) {
         // Edge case
         if(head->next == NULL){
@@ -35,4 +37,26 @@ public:
         return temp;
 
     }
+    */
+
+    // 2nd approach ex agar koi admi 2m se chal raha hai and 2nd person 1m se chal raha hai to after 10 min pehla wala banda 20m cover kar lega and doosra banda 10m cover karlega aur bilkul middle pe hogga . same goes with nodes traversal
+
+    ListNode* middleNode(ListNode* head){
+        if(head == NULL || head->next == NULL){
+            return head;
+        }
+        ListNode * slow = head;
+        ListNode * fast = head->next;
+
+        while(fast != NULL){
+            fast = fast->next;
+            if(fast != NULL){
+                fast = fast->next; 
+            }
+
+            slow = slow -> next;
+        }
+        return slow;
+    }
+
 };
