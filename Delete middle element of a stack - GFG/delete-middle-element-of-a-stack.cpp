917e86e249403c979a4e37f124358a8ca2a86446
omkumarjha <88018877+omkumarjha@users.noreply.github.com>
2023-07-30 16:19:31 +0530

    Added solution - LeetHub

--- Delete middle element of a stack - GFG/delete-middle-element-of-a-stack.cpp
+++ Delete middle element of a stack - GFG/delete-middle-element-of-a-stack.cpp
@@ -11,7 +11,10 @@ class Solution
 {
     public:
     //Function to delete middle element of a stack.
+    /*
     void deleteMid(stack<int>&s, int sizeOfStack){
+        // iss approach mai basically humne middle element se uper ke sare element doosre stack mai dale then middle ko hataya then fir temp ke elements   ko one by one original stack mai dala 
+        // Yeh extra space le raha hai 
         int count = floor(s.size()/2);
         
         stack<int> temp ;
@@ -29,6 +32,35 @@ class Solution
         }
         
     }
+    */
+    
+    // Below approach recursion ka use karke 0(1) space mai question ko solve kar rah hai .
+    void solve(stack<int> &s,int count,int size){
+        // sare ke sare function call stack ki same value ko share karte hai .
+        
+        // base case
+        if(count == size/2){
+            s.pop(); // yeh middle element ko pop out karega
+            return;
+        }
+        else{
+            int num = s.top();
+            s.pop();
+            
+            // Aage ke liye recursive call
+            solve(s,count+1,size);
+
+            s.push(num);
+        }
+        
+    }
+    
+    void deleteMid(stack<int>&s, int sizeOfStack){
+        int count = 0;
+        
+        solve(s,count,sizeOfStack);
+    }
+    
 };
 
 //{ Driver Code Starts.
