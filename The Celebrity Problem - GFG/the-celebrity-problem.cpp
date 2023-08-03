4712a1ea0ad1c92ff771924acdc27ea3a8f0f130
omkumarjha <88018877+omkumarjha@users.noreply.github.com>
2023-08-03 17:08:30 +0530

    Added solution - LeetHub

--- The Celebrity Problem - GFG/the-celebrity-problem.cpp
+++ The Celebrity Problem - GFG/the-celebrity-problem.cpp
@@ -11,35 +11,48 @@ class Solution
 {
     public:
 
+    // Below approach ki TC 0(n^2) hai
+    
+    // Main keywords 
+    // 1. All people knows celebrity means celebrity ki column ke number of 1s == n-1
+    // 2. celebrity does not know anyone at a party , menas celebrity ki row ke number of zeroes == 0
+
     int celebrity(vector<vector<int> >& M, int n){
-        int one = 0;
-        int ans = -1;
+        int one = 0 , nonZero = 0;
         
         for(int i = 0; i < n; i++){
             one = 0;
+            nonZero = 0; // below loop se mai number of one count karr rha hoon ith column mai
             for(int j = 0; j < n; j++){
                 if(M[j][i] == 1){
                     one++;
                 }
             }
-            if(one == n-1){
-                ans = i;
-                break;
-            }
-        }
-        
-        if(ans != -1){
-            for(int i = 0; i < n; i++){
-                if(M[ans][i] == 1){
-                    return -1;
+            // below loop se mai ith row ke number of zeroes count kar raha hoon 
+            for(int k = 0; k < n; k++){
+                if(M[i][k] == 1){
+                    nonZero++;
                 }
             }
-            return ans;
+            
+            // condition for celebrity 
+            if(one == n-1 && nonZero == 0){
+                return i;
+            }
+            
         }
         
         return -1;
-        
     }
+
+    // Below approach TC 0(n) hai
+    
+    // int celebrity(vector<vector<int> >& M, int n){  
+        
+        
+        
+        
+    // }
 };
 
 //{ Driver Code Starts.
