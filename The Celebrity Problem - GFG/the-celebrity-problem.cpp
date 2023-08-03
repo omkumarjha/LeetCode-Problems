7e0b9998713859ebe2796c17a8551a69d48812fa
omkumarjha <88018877+omkumarjha@users.noreply.github.com>
2023-08-03 19:19:24 +0530

    Added solution - LeetHub

--- The Celebrity Problem - GFG/the-celebrity-problem.cpp
+++ The Celebrity Problem - GFG/the-celebrity-problem.cpp
@@ -17,6 +17,7 @@ class Solution
     // 1. All people knows celebrity means celebrity ki column ke number of 1s == n-1
     // 2. celebrity does not know anyone at a party , menas celebrity ki row ke number of zeroes == 0
 
+    /*
     int celebrity(vector<vector<int> >& M, int n){
         int one = 0 , nonZero = 0;
         
@@ -44,15 +45,73 @@ class Solution
         
         return -1;
     }
+    */
 
-    // Below approach TC 0(n) hai
+    // Below approach TC 0(n) ka hai and it uses stack 
     
-    // int celebrity(vector<vector<int> >& M, int n){  
+    bool knows(vector<vector<int> >& M, int a, int b, int n) {
+        if(M[a][b] == 1)
+            return true;
+        else
+            return false;
+    }
+
+    int celebrity(vector<vector<int> >& M, int n) {
         
+        stack<int> s;
+        //step1: push all element in stack
+        for(int i=0; i<n; i++) {
+            s.push(i);
+        }   
         
+        //step2: get 2 elements at a time and compare them
         
+        while(s.size() > 1) {
+            
+            int a = s.top();
+            s.pop();
+            
+            int b = s.top();
+            s.pop();
+            
+            if(knows(M,a,b,n)){ // a agar b ko janta hai means a to celebrity hoga nhi 
+                s.push(b);
+            }
+            else{  // aaisa bhi case ho sakta hai jisme a and b koi eak doosre ko naa jaane.
+                s.push(a);
+            }
+        }
         
-    // }
+        int ans = s.top();
+        //step3: single element in stack is potential celebrity 
+        //so verify it
+        
+        int zeroCount = 0;
+        
+        for(int i=0; i<n; i++) {
+            if(M[ans][i] == 0)
+                zeroCount++;
+        }
+        
+        //all zeroes
+        if(zeroCount != n)
+            return -1;
+        
+        //column check
+        int oneCount = 0;
+        
+        for(int i=0; i<n; i++) {
+            if(M[i][ans] == 1)
+                oneCount++;
+        }
+        
+        if(oneCount != n-1)
+            return -1;
+        
+        return ans;
+    
+    }
+        
 };
 
 //{ Driver Code Starts.
