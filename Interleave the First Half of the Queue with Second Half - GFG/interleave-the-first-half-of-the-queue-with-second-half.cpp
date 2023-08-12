5f6cf29e3455e6072a98394e7322b2afda7a1ac5
omkumarjha <88018877+omkumarjha@users.noreply.github.com>
2023-08-12 19:35:07 +0530

    Added solution - LeetHub

--- Interleave the First Half of the Queue with Second Half - GFG/interleave-the-first-half-of-the-queue-with-second-half.cpp
+++ Interleave the First Half of the Queue with Second Half - GFG/interleave-the-first-half-of-the-queue-with-second-half.cpp
@@ -18,8 +18,6 @@ public:
         step 2 : then jab tak newq empty nhi ho jaata tab tak newQ ke front ko normalq mai push karo and then normalq ke front ko normalq ke andar push karo 
         
         
-        */
-        
         queue<int> newq;
         vector<int> ans;
         
@@ -45,7 +43,8 @@ public:
             q.push(val);
         }
         
-        coping value
+        // coping values from normal queue to our vector array
+        
         while(!q.empty()){
             int val = q.front();
             q.pop();
@@ -54,6 +53,62 @@ public:
         
         return ans;
         
+        
+        */
+        
+        // 2nd approach by using stack agar interviewer ne bola hai to otherwise uper wala approach optimized and easy hai iska bhi spaceC 0(n) hoga.
+        
+        /*
+        step 1 : fetch first help from queue and push it into the stack
+        step 2 : now stack jab tak empty na hota tab tak queue mai uske top of the element ko push karo 
+        step 3 : Abb queue ke first half of the elements ko normal queue ke end mai dalo 
+        step 4 : Again normal queue ke first half of the elements ko stack mai dalo .
+        step 5 : Now jab tak stack empty na hojaye tab tak pehle to stack ka top normal queue ke andar dalo then normal queue ka front element normal queue ke back mai dl
+        */
+        
+        vector<int> ans;
+        stack<int> temp;
+        
+        int firstHalf = q.size()/2;
+        
+        // step 1
+        for(int i = 0; i < firstHalf; i++){
+            temp.push(q.front());
+            q.pop();
+        }
+        
+        // step 2
+        while(!temp.empty()){
+            q.push(temp.top());
+            temp.pop();
+        }
+        
+        // step 3 
+        for(int i = 0; i < firstHalf; i++){
+            q.push(q.front());
+            q.pop();
+        }
+        
+        // step 4
+         for(int i = 0; i < firstHalf; i++){
+            temp.push(q.front());
+            q.pop();
+        }
+        
+        // step 5
+        
+        while(!temp.empty()){
+            int val = temp.top();
+            temp.pop();
+            ans.push_back(val);
+            
+            val = q.front();
+            q.pop();
+            ans.push_back(val);
+        }
+        
+        return ans;
+      
     }
 };
 
