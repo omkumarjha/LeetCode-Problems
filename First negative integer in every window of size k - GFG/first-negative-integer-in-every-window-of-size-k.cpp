6a7d19fa6a1733975647d929730eb5beb9d26847
omkumarjha <88018877+omkumarjha@users.noreply.github.com>
2023-08-30 09:23:38 +0530

    Added solution - LeetHub

--- First negative integer in every window of size k - GFG/first-negative-integer-in-every-window-of-size-k.cpp
+++ First negative integer in every window of size k - GFG/first-negative-integer-in-every-window-of-size-k.cpp
@@ -29,6 +29,8 @@ int main() {
 // } Driver Code Ends
 
 
+
+
 vector<long long> printFirstNegativeInteger(long long int A[],long long int N, long long int K) {
     
         // Below approach mai hum window by window search kar rahe hai par iski worst case mai TC 0(n*2) ho sakti hai so we hae to optimize this.
@@ -55,13 +57,13 @@ vector<long long> printFirstNegativeInteger(long long int A[],long long int N, l
         // Optimized approach by Love Babbar isme hum pehli k size ke window ko process karke first -ve integer ans mai dal denge and next mai aage ke windows ko process karenge 
         
         vector<long long> ans;
-        deque<int> dq;
+        queue<int> q;
         
         // pehli window ko process karo 
         
         for(int i = 0; i < K; i++){
             if(A[i] < 0){
-                dq.push_back(i);
+                dq.push(i);
             }
         }
         
@@ -78,13 +80,13 @@ vector<long long> printFirstNegativeInteger(long long int A[],long long int N, l
             // Remove karo dq ka front agar woh curr window mai lie nhi kar raha to understand (i-.front) >= k refer example 1 with k = 3 .
             
             if(!dq.empty() && (i-dq.front()) >= K){ // agar yeh formula true hota hai iska matlab dq ka front curr window mai lie nhi kar raha 
-                dq.pop_front();
+                dq.pop();
             }
             
             // abb curr element ko check karenge 
             
             if(A[i] < 0){
-                dq.push_back(i);
+                dq.push(i);
             }
             
             // Now ans mai -ve integer dalo 
