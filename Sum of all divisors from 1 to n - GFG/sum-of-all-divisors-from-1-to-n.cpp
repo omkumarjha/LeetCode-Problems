1d1de094ec779bb3920fd96f3fe2466d120c45fb
Om Kumar Jha <88018877+omkumarjha@users.noreply.github.com>
2023-10-21 19:36:02 +0530

    Added solution - LeetHub

--- Sum of all divisors from 1 to n - GFG/sum-of-all-divisors-from-1-to-n.cpp
+++ Sum of all divisors from 1 to n - GFG/sum-of-all-divisors-from-1-to-n.cpp
@@ -24,15 +24,25 @@ public:
         
         // Below approach is by 'Real and Imaginory' YT channel TC - 0(n) and SC - 0(1)
         
+        // long long ans = 0;
+        
+        // for(int i = 1; i <= N; i++){
+        //     if(N % i == 0){
+        //         ans += N; // To iss case mai N ans mai pura contribution dega
+        //     }
+        //     else{
+        //         ans += i *(N/i); // eg 6 ko 5 completely divide nhi kar sakta so in this case we will store 5 * (6/5) = 5.
+        //     }
+        // }
+        
+        // return ans;
+        
+        // Last approach By Om TC - 0(n) and sc = 0(1)
+        
         long long ans = 0;
         
         for(int i = 1; i <= N; i++){
-            if(N % i == 0){
-                ans += N; // To iss case mai N ans mai pura contribution dega
-            }
-            else{
-                ans += i *(N/i); // eg 6 ko 5 completely divide nhi kar sakta so in this case we will store 5 * (6/5) = 5.
-            }
+            ans += i * (N/i);
         }
         
         return ans;
